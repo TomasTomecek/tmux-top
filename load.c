4cbdae841036ac4a22ef8c7d6ac388901480de20
Tomas Tomecek <ttomecek@redhat.com>
2014-10-14 22:22:35 +0200

    finish static load display
    dynamic (via args) is still not working

--- load.c
+++ load.c
@@ -41,89 +41,105 @@ get_load(void) {
     return load_s;
 }
 
-struct load_entry**
-init_load_list(char *args[], int count) {
-    int to_alloc = 3;
-    if (count > 0) {
-        to_alloc = count;
-    }
+int cmp_func(const void * a, const void * b) {
+    struct load_entry *a_s = (struct load_entry *) a;
+    struct load_entry *b_s = (struct load_entry *) b;
+    return (a_s->load_d < b_s->load_d) - (a_s->load_d > b_s->load_d);
+}
 
-    struct load_entry **entries = calloc(to_alloc, sizeof(struct load_entry*));
+struct load_entry**
+init_load_list(char *args[], int count, int *entries_count) {
+    *entries_count = 3;
+    if (count > 0) {
+        *entries_count = count;
+    }
+    printf("after if");
+
+    struct load_entry **entries = calloc(*entries_count, sizeof(struct load_entry*));
 
     if (count == 0) {
         entries[0] = (struct load_entry*) malloc(sizeof(struct load_entry));
-        entries[0]->load_d = 0.05;
+        entries[0]->load_d = 0.25;
         entries[0]->bg = "default";
         entries[0]->fg = "green";
         entries[1] = (struct load_entry*) malloc(sizeof(struct load_entry));
-        entries[1]->load_d = 0.1;
+        entries[1]->load_d = 0.5;
         entries[1]->bg = "default";
-        entries[1]->fg = "orange";
+        entries[1]->fg = "colour166";  // solarized orange
         entries[2] = (struct load_entry*) malloc(sizeof(struct load_entry));
-        entries[2]->load_d = 0.5;
+        entries[2]->load_d = 1.0;
         entries[2]->bg = "default";
-        entries[2]->fg = "red";
-        return entries;
-    }
-
-    char *token, *string;
-    char *triple[3];
-    int loop_iteration;
-    for (int i = 0; i < count; i++) {
-        string = strdup(args[i]);
-        loop_iteration = 0;
-        while ((token = strsep(&string, ",")) != NULL) {
-            triple[i] = token;
-            ++loop_iteration;
+        entries[2]->fg = "colour1"; // solarzied red
+    } else {
+        char *token, *string;
+        char *triple[3];
+        int loop_iteration;
+        for (int i = 0; i < count; i++) {
+            string = strdup(args[i]);
+            printf("%s/n", string);
+            loop_iteration = 0;
+            while ((token = strsep(&string, ",")) != NULL) {
+                triple[i] = token;
+                ++loop_iteration;
+            }
+            entries[i] = (struct load_entry*) malloc(sizeof(struct load_entry));
+            entries[i]->fg = triple[1];
+            sscanf(triple[0], "%lf", &entries[i]->load_d);
+            if (loop_iteration == 3) {
+                entries[i]->bg = triple[2];
+            } else {
+                entries[i]->bg = "";
+            }
         }
-        entries[i] = (struct load_entry*) malloc(sizeof(struct load_entry));
-        entries[i]->fg = triple[1];
-        sscanf(triple[0], "%lf", &entries[i]->load_d);
-        if (loop_iteration == 3) {
-            entries[i]->bg = triple[2];
-        } else {
-            entries[i]->bg = "";
-        }
     }
+    // sort array
+    qsort(entries, *entries_count, sizeof(struct load_entry*), cmp_func);
     return entries;
 }
 
 void
-print_load(struct **load_entry entries, int count) {
-    unsigned int number_of_cpus = sysconf(_SC_NPROCESSORS_ONLN);
-    double cpus_d = (double) number_of_cpus;
-    struct loadavg = get_load();
-
-    printf("%f\n", cpus_d);
-    printf("%f\n", loadavg.one_min / cpus_d * 100.0 );
-
+print_load_item(struct load_entry **entries, int count, double cpus_d, double load_to_display, char * suffix) {
+    // when you load/#cpus, you get an absolute load: lets not do that
+    //double absolute_load = load_to_display / cpus_d * 100.0;
     for (int i = 0; i < count; i++) {
-        if (loadavg.one_min / cpus_d * 100.0 > LOAD_HIGH ) {
-            printf("High load: %.2f\n", loadavg.one_min);
-        } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_MED ) {
-            printf("Medium load: %.2f\n", loadavg.one_min);
-        } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_LOW ) {
-            printf("Low load: %.2f\n", loadavg.one_min);
-        } else {
-            printf("Minimal load: %.2f\n", loadavg.one_min);
+        // printf("\n[%d] load = %.2f, threshold = %.2f\n", i, load_to_display, entries[i]->load_d);
+        if (load_to_display > entries[i]->load_d || i == count - 1) {
+            if (strcmp(entries[i]->bg, "") == 0) {
+                printf("#[fg=%s]%.2f#[fg=default]%s",
+                       entries[i]->fg, load_to_display, suffix);
+            } else {
+                printf("#[bg=%s,fg=%s]%.2f#[fg=default,bg=default]%s",
+                       entries[i]->bg, entries[i]->fg, load_to_display, suffix);
+            }
+            return;
         }
     }
+}
 
+void
+print_load(struct load_entry **entries, int count) {
+    unsigned int number_of_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    double cpus_d = (double) number_of_cpus;
+    struct load loadavg = get_load();
+
+    print_load_item(entries, count, cpus_d, loadavg.one_min, " ");
+    print_load_item(entries, count, cpus_d, loadavg.five_min, " ");
+    print_load_item(entries, count, cpus_d, loadavg.fifteen_min, "");
 }
 
 /* ARGPARSE */
 
 const char *argp_program_version =
   "load 0.1";
+
 const char *argp_program_bug_address =
   "github.com/TomasTomecek/tmux-status-helpers/";
 
 /* Program documentation. */
-static char doc[] =
-  "doc";
+static char doc[] = "doc";
 
 /* A description of the arguments we accept. */
-static char args_doc[] = "[THRESHOLD_ENTRY THRESHOLD_ENTRY...]";
+static char args_doc[] = "[THRESHOLD_ENTRY [THRESHOLD_ENTRY...]]";
 
 /* The options we understand. */
 static struct argp_option options[] = {
@@ -137,7 +153,6 @@ struct arguments
   int args_count;
 };
 
-/* Parse a single option. */
 static error_t
 parse_opt (int key, char *arg, struct argp_state *state)
 {
@@ -161,12 +176,9 @@ parse_opt (int key, char *arg, struct argp_state *state)
   return 0;
 }
 
-/* Our argp parser. */
 static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
-
 /* MAIN */
-
 
 int
 main(int argc, char **argv) {
@@ -174,8 +186,10 @@ main(int argc, char **argv) {
     arguments.args_count = 0;
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    struct **load_entry entries = init_load_list(arguments.args, arguments.args_count);
-    print_load(entries, 3);
+    int entries_count = 0;
+    printf("#args = %d\n", arguments.args_count);
+    struct load_entry **entries = init_load_list(arguments.args, arguments.args_count, &entries_count);
+    print_load(entries, entries_count);
 
     return 0;
 }
