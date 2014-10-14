6a57c2a63efa538707ce3edab20f1412a77cc501
Tomas Tomecek <ttomecek@redhat.com>
2014-10-14 17:32:08 +0200

    save

--- load.c
+++ load.c
@@ -6,26 +6,26 @@
 #include <string.h>  // strsep
 
 
-typedef struct load {
+struct load {
     double one_min;
     double five_min;
     double fifteen_min;
-} load;
+};
 
 
 // how to display specific interval
-typedef struct load_entry_s {
+struct load_entry {
     double load_d;
     char * bg;
     char * fg;
-} load_entry;
+};
 
 
-load
+struct load
 get_load(void) {
     signed int is_error;
     double loadavg[3];
-    load load_s;
+    struct load load_s;
 
     is_error = getloadavg(loadavg, 3);
 
@@ -41,21 +41,28 @@ get_load(void) {
     return load_s;
 }
 
-load_entry*
+struct load_entry**
 init_load_list(char *args[], int count) {
     int to_alloc = 3;
     if (count > 0) {
         to_alloc = count;
     }
 
-    load_entry entries[to_alloc];
+    struct load_entry **entries = calloc(to_alloc, sizeof(struct load_entry*));
 
     if (count == 0) {
-        entries = {
-            { 0.05, "default", "green" },
-            { 0.05, "default", "green" },
-            { 0.20, "default", "red" }
-        }
+        entries[0] = (struct load_entry*) malloc(sizeof(struct load_entry));
+        entries[0]->load_d = 0.05;
+        entries[0]->bg = "default";
+        entries[0]->fg = "green";
+        entries[1] = (struct load_entry*) malloc(sizeof(struct load_entry));
+        entries[1]->load_d = 0.1;
+        entries[1]->bg = "default";
+        entries[1]->fg = "orange";
+        entries[2] = (struct load_entry*) malloc(sizeof(struct load_entry));
+        entries[2]->load_d = 0.5;
+        entries[2]->bg = "default";
+        entries[2]->fg = "red";
         return entries;
     }
 
@@ -69,37 +76,40 @@ init_load_list(char *args[], int count) {
             triple[i] = token;
             ++loop_iteration;
         }
-        entries[i] = (load_entry) { .load_d=0.0, .bg="", .fg=triple[1] };
-        sscanf(triple[0], "%lf", &entries[i].load_d);
+        entries[i] = (struct load_entry*) malloc(sizeof(struct load_entry));
+        entries[i]->fg = triple[1];
+        sscanf(triple[0], "%lf", &entries[i]->load_d);
         if (loop_iteration == 3) {
-            entries[i].bg = triple[2];
+            entries[i]->bg = triple[2];
+        } else {
+            entries[i]->bg = "";
         }
     }
     return entries;
 }
-/*
+
 void
-print_load(load loadavg) {
+print_load(struct **load_entry entries, int count) {
     unsigned int number_of_cpus = sysconf(_SC_NPROCESSORS_ONLN);
     double cpus_d = (double) number_of_cpus;
+    struct loadavg = get_load();
 
     printf("%f\n", cpus_d);
     printf("%f\n", loadavg.one_min / cpus_d * 100.0 );
 
-    if (loadavg.one_min / cpus_d * 100.0 > LOAD_HIGH ) {
-        printf("High load: %.2f\n", loadavg.one_min);
-    } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_MED ) {
-        printf("Medium load: %.2f\n", loadavg.one_min);
-    } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_LOW ) {
-        printf("Low load: %.2f\n", loadavg.one_min);
-    } else {
-        printf("Minimal load: %.2f\n", loadavg.one_min);
-
+    for (int i = 0; i < count; i++) {
+        if (loadavg.one_min / cpus_d * 100.0 > LOAD_HIGH ) {
+            printf("High load: %.2f\n", loadavg.one_min);
+        } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_MED ) {
+            printf("Medium load: %.2f\n", loadavg.one_min);
+        } else if (loadavg.one_min / cpus_d * 100.0 > LOAD_LOW ) {
+            printf("Low load: %.2f\n", loadavg.one_min);
+        } else {
+            printf("Minimal load: %.2f\n", loadavg.one_min);
+        }
     }
 
 }
-*/
-
 
 /* ARGPARSE */
 
@@ -160,20 +170,12 @@ static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
 
 int
 main(int argc, char **argv) {
-    //load_entry * entries = malloc(300 * sizeof(load_entry));
-    //entries = &(load_entry) { .load_d=0.05, .bg="default", .fg="green" };
-    //printf("%f", entries->load_d);
-    //*(entries + 1) = (load_entry) { .load_d=0.05, .bg="default", .fg="green" };
-    //*(entries + 2) = (load_entry) { .load_d=0.2, .bg="default", .fg="red" };
-    //init_load_list(&entries);
-
-    //print_load(get_load());
-
     struct arguments arguments;
     arguments.args_count = 0;
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-    init_load_list(arguments.args, arguments.args_count);
+    struct **load_entry entries = init_load_list(arguments.args, arguments.args_count);
+    print_load(entries, 3);
 
     return 0;
 }
