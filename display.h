5ac99e0d057d2ec0dc52261a3a1ff60bebbd23dd
Tomas Tomecek <ttomecek@redhat.com>
2015-02-02 23:59:40 +0100

    remove c code

--- display.h
+++
@@ -1,9 +0,0 @@
-// how do you display specific interval
-struct interval_display {
-    double threshold;
-    char * bg;
-    char * fg;
-};
-
-struct interval_display** init_interval_list(const char *args[], int count);
-void print_interval_item(struct interval_display **entries, int count, double value, char * suffix);
