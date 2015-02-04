5a5428d211916d65080ebe32975f23a0f2defe83
Tomas Tomecek <ttomecek@redhat.com>
2015-02-04 22:58:57 +0100

    PoC net

--- src/tmux-top/cli/cli.go
+++ src/tmux-top/cli/cli.go
@@ -7,6 +7,7 @@ import (
 	"tmux-top/conf"
 	"tmux-top/display"
 	"tmux-top/load"
+	"tmux-top/net"
 )
 
 var configuration conf.Configuration = conf.LoadConf()
@@ -19,12 +20,26 @@ func print_load(*cli.Context) {
 		tmux_display.DisplayFloat64(fifteen, 2, configuration.Load.Intervals))
 }
 
+func print_net(*cli.Context) {
+	net_stats := net.GetNetStats()
+	fmt.Printf("%s %s %s",
+		tmux_display.DisplayFloat64(one, 2, configuration.Load.Intervals),
+		tmux_display.DisplayFloat64(five, 2, configuration.Load.Intervals),
+		tmux_display.DisplayFloat64(fifteen, 2, configuration.Load.Intervals))
+}
+
 func main() {
 
 	app := cli.NewApp()
 	app.Name = "boom"
 	app.Usage = "make an explosive entrance"
 	app.Commands = []cli.Command{
+		{
+			Name:      "net",
+			ShortName: "n",
+			Usage:     "show net stats ",
+			Action:    print_net,
+		},
 		{
 			Name:      "load",
 			ShortName: "l",
