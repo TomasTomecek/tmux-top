a29fcbdc534e757305bf3d079750f585170f4188
Tomas Tomecek <ttomecek@redhat.com>
2015-02-11 15:19:09 +0100

    add basic tests for conf

---
+++ conf/conf_test.go
@@ -0,0 +1,28 @@
+package conf
+
+import (
+	"fmt"
+	"testing"
+)
+
+// func loadIntervalValue(s string) (float64, bool) {
+
+func TestLoadIntervalValue1(t *testing.T) {
+	d, r := loadIntervalValue("1.0")
+	if d != 1.0 {
+		t.Error(fmt.Sprintf("%f != 1.0", d))
+	}
+	if r {
+		t.Error("Value should be absolute!")
+	}
+}
+
+func TestLoadIntervalValue2(t *testing.T) {
+	d, r := loadIntervalValue("10%")
+	if d != 0.1 {
+		t.Error(fmt.Sprintf("%f != 0.1", d))
+	}
+	if !r {
+		t.Error(fmt.Errorf("Value '%f' should be relative!", d))
+	}
+}
