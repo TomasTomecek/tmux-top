34a6ef7be25fcfba59e84ea938f5f444907c56d0
Tomas Tomecek <ttomecek@redhat.com>
2015-02-12 09:49:32 +0100

    mem used = total - free - cached - buffers
    Fixes #1

--- mem/mem.go
+++ mem/mem.go
@@ -10,7 +10,7 @@ import (
 )
 
 func GetMemStats() (used, total float64) {
-	var free float64
+	var free, buffers, cached float64
 	contents, err := ioutil.ReadFile("/proc/meminfo")
 	if err != nil {
 		return
@@ -26,13 +26,18 @@ func GetMemStats() (used, total float64) {
 			// no idea why /proc/meminfo outputs stuff in kilobytes
 			// what is this, 1990?
 			total, _ = humanize.Dehumanize(total, line_chunks[2])
-		}
-		if line_chunks[0] == "MemFree" {
+		} else if line_chunks[0] == "MemFree" {
 			free, _ = strconv.ParseFloat(line_chunks[1], 64)
 			free, _ = humanize.Dehumanize(free, line_chunks[2])
+		} else if line_chunks[0] == "Buffers" {
+			buffers, _ = strconv.ParseFloat(line_chunks[1], 64)
+			buffers, _ = humanize.Dehumanize(buffers, line_chunks[2])
+		} else if line_chunks[0] == "Cached" {
+			cached, _ = strconv.ParseFloat(line_chunks[1], 64)
+			cached, _ = humanize.Dehumanize(cached, line_chunks[2])
 		}
 	}
-	used = total - free
+	used = total - free - cached - buffers
 	return
 }
 
